e5a8199ad302a459ac858e962fc2349646e677df
Blaž Hrastnik <blaz@mxxn.io>
2024-02-19 01:30:04 +0900

    Migrate to qmk userspace

--- keyboards/ergodox_ez/keymaps/archseer/config.h
+++ keyboards/ergodox_ez/keymaps/archseer/config.h
@@ -3,7 +3,10 @@
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 3
 // left board started having double inputs
-// #undef DEBOUNCE
-// #define DEBOUNCE 45
+#undef DEBOUNCE
+#define DEBOUNCE 45
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// this is default?
+#define TAPPING_TERM 200
