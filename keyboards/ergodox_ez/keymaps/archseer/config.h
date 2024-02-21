5afe5d9525711cb7e330bc3d5a18a7aab5d55ebb
Blaž Hrastnik <blaz@mxxn.io>
2024-02-21 22:55:38 +0900

    ergodox_ez: Still typing issues without debounce

--- keyboards/ergodox_ez/keymaps/archseer/config.h
+++ keyboards/ergodox_ez/keymaps/archseer/config.h
@@ -3,8 +3,8 @@
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 3
 // left board started having double inputs
-// #undef DEBOUNCE
-// #define DEBOUNCE 45
+#undef DEBOUNCE
+#define DEBOUNCE 45
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
